f18b3a51620f726f32e4858dac1574563e387fe8
Blake A. Long <blake.a.long@gmail.com>
2020-03-16 22:54:37 -0600

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(response => response.json())
+    .then(books => renderBooks(books))
 }
 
 function renderBooks(json) {
@@ -8,9 +10,9 @@ function renderBooks(json) {
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
-  })
+  } )
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
-})
+} )
